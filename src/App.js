652b88c82231914e407144e29d79b38bbed1201b
SirajB <sirajbrepotra@gmail.com>
2019-05-22 11:20:36 +0100

    Imported openWeather API along with images which displays within Dashboard.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import makeFarm from "./p5Setup"
 import Farm from "./models/Farm"
 import Farmer from "./models/Farmer"
 import Market from "./models/Market"
+import request from "request"
+
+const apiId = "d25ed5bd07243e88a2c2eee1e17cba04"
 
 class App extends Component {
   // all instances live on the state
   state = {
     farmer: new Farmer(),
     farm: new Farm(),
-    market: new Market()
+    market: new Market(),
+    weatherIcon: Boolean = false
   }
 
   // allow instances to to tell us when they change
@@ -31,6 +35,24 @@ class App extends Component {
     this.setState({
       myP5: new p5(sketch, "sketch")
     })
+
+    this.weather()
+  }
+
+  weather() {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${apiId}`
+    request(url, (err, response, body) => {
+      if (err) {
+        console.log("Error: ", err)
+      } else {
+        console.log("Worked")
+        const json = JSON.parse(body)
+
+        this.setState({ imgSrc: `http://openweathermap.org/img/w/${json.weather[0].icon}.png` })
+        this.setState({ value: (`${Math.round(json.main.temp - 273.15)}°C ${json.weather[0].main}`) })
+        this.setState({ showWeatherIcon: true })
+      }
+    })
   }
 
   render() {
@@ -41,6 +63,10 @@ class App extends Component {
             <img src="/img/farmer.png" className="App-logo" alt="logo" />{" "}
             Dashboard
           </h2>
+          <div className="Weather">
+            <h3 ref="WeatherTemperature">{this.state && this.state.value}</h3>
+            {this.state.showWeatherIcon && <img ref="WeatherIcon" alt="Weather" src={this.state.imgSrc}></img>}
+          </div>
         </header>
         <FarmManager farmer={this.state.farmer} farm={this.state.farm} market={this.state.market} />
       </div>
