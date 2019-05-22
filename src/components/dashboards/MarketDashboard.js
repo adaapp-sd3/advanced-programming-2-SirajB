02600440f2ceb2b381b331d19c09631ac839e648
SirajB <sirajbrepotra@gmail.com>
2019-05-22 10:44:48 +0100

    small code cleanup

--- src/components/dashboards/MarketDashboard.js
+++ src/components/dashboards/MarketDashboard.js
@@ -12,6 +12,7 @@ class MarketDashboard extends Component {
         currentFarmer.myFarm.seeds.total += 1
         currentFarmer.budget -= this.props.market.grassSeedPrice
         console.log(currentFarmer.myFarm.seeds)
+
       }
 
     } else if (item === "solarPanel") {
@@ -38,8 +39,6 @@ class MarketDashboard extends Component {
         currentFarmer.budget -= this.props.market.animalCowPrice
         console.log(currentFarmer.myFarm.greenGas)
       }
-
-
     } else if (item === "chickens") {
       if (currentFarmer.budget - this.props.market.animalChickenPrice > 0) {
 
@@ -47,8 +46,6 @@ class MarketDashboard extends Component {
         currentFarmer.budget -= this.props.market.animalChickenPrice
         console.log(currentFarmer.myFarm.greenGas)
       }
-
-
     } else if (item === "sheep") {
       if (currentFarmer.budget - this.props.market.animalSheepPrice > 0) {
 
@@ -56,10 +53,7 @@ class MarketDashboard extends Component {
         currentFarmer.budget -= this.props.market.animalSheepPrice
         console.log(currentFarmer.myFarm.greenGas)
       }
-
-
     }
-
   }
 
   sellItem = item => {
@@ -101,69 +95,69 @@ class MarketDashboard extends Component {
   render() {
     return (
       <div className="MarketDashboard">
-        <h2>Market</h2>
-        <p>Welcome to the market! See our prices below:</p>
-        <h3>Buy</h3>
-        <dl>
-          <dt>Seeds</dt>
-          <dd>
-            <button onClick={() => this.buyItem("seeds")}>
-              Buy seeds for £{this.props.market.grassSeedPrice}
-            </button>
-          </dd>
-          <dt>Solar panels</dt>
-          <dd>
-            <button onClick={() => this.buyItem("solarPanel")}>
-              £{this.props.market.solarPanelPrice} per unit
-            </button>
-          </dd>
-          <dt>Green gas</dt>
-          <dd>
-            <button onClick={() => this.buyItem("greenGas")}>
-              £{this.props.market.greenGasPrice} per unit
-            </button>
-          </dd>
-          <dt>Cows</dt>
-          <dd>
-            <button onClick={() => this.buyItem("cows")}>
-              £{this.props.market.animalCowPrice} per animal
-            </button>
-          </dd>
-          <dt>Chickens</dt>
-          <dd>
-            <button onClick={() => this.buyItem("chickens")}>
-              £{this.props.market.animalChickenPrice} per animal
-            </button>
-          </dd>
-          <dt>Sheep</dt>
-          <dd>
-            <button onClick={() => this.buyItem("sheep")}>
-              £{this.props.market.animalSheepPrice} per animal
-            </button>
-          </dd>
-        </dl>
-        <h3>Sell</h3>
-        <dl>
-          <dt>Milk</dt>
-          <dd>£{this.props.market.milkPrice} per pint</dd>
-          <button onClick={() => this.sellItem("milk")}>Sell Milk</button>
-          <dt>Beef</dt>
-          <dd>£{this.props.market.beefPrice} ounce</dd>
-          <button onClick={() => this.sellItem("beef")}>Sell Beef</button>
-          <dt>Eggs</dt>
-          <dd>£{this.props.market.eggPrice} ounce</dd>
-          <button onClick={() => this.sellItem("egg")}>Sell Eggs</button>
-          <dt>Chicken</dt>
-          <dd>£{this.props.market.meatChickenPrice} kilograms</dd>
-          <button onClick={() => this.sellItem("chicken")}>Sell Chicken</button>
-          <dt>Wool</dt>
-          <dd>£{this.props.market.woolPrice} bags</dd>
-          <button onClick={() => this.sellItem("wool")}>Sell Wool</button>
-          <dt>Lamb</dt>
-          <dd>£{this.props.market.meatLambPrice} kilograms</dd>
-          <button onClick={() => this.sellItem("lamb")}>Sell Lamb</button>
-        </dl>
-      </div>
+      <h2>Market</h2>
+      <p>Welcome to the market! See our prices below:</p>
+      <h3>Buy</h3>
+      <dl>
+        <dt>Seeds</dt>
+        <dd>
+          <button onClick={() => this.buyItem("seeds")}>
+            Buy seeds for £{this.props.market.grassSeedPrice}
+          </button>
+        </dd>
+        <dt>Solar panels</dt>
+        <dd>
+          <button onClick={() => this.buyItem("solarPanel")}>
+            £{this.props.market.solarPanelPrice} per unit
+          </button>
+        </dd>
+        <dt>Green gas</dt>
+        <dd>
+          <button onClick={() => this.buyItem("greenGas")}>
+            £{this.props.market.greenGasPrice} per unit
+          </button>
+        </dd>
+        <dt>Cows</dt>
+        <dd>
+          <button onClick={() => this.buyItem("cows")}>
+            £{this.props.market.animalCowPrice} per animal
+          </button>
+        </dd>
+        <dt>Chickens</dt>
+        <dd>
+          <button onClick={() => this.buyItem("chickens")}>
+            £{this.props.market.animalChickenPrice} per animal
+          </button>
+        </dd>
+        <dt>Sheep</dt>
+        <dd>
+          <button onClick={() => this.buyItem("sheep")}>
+            £{this.props.market.animalSheepPrice} per animal
+          </button>
+        </dd>
+      </dl>
+      <h3>Sell</h3>
+      <dl>
+        <dt>Milk</dt>
+        <dd>£{this.props.market.milkPrice} per pint</dd>
+        <button onClick={() => this.sellItem("milk")}>Sell Milk</button>
+        <dt>Beef</dt>
+        <dd>£{this.props.market.beefPrice} ounce</dd>
+        <button onClick={() => this.sellItem("beef")}>Sell Beef</button>
+        <dt>Eggs</dt>
+        <dd>£{this.props.market.eggPrice} ounce</dd>
+        <button onClick={() => this.sellItem("egg")}>Sell Eggs</button>
+        <dt>Chicken</dt>
+        <dd>£{this.props.market.meatChickenPrice} kilograms</dd>
+        <button onClick={() => this.sellItem("chicken")}>Sell Chicken</button>
+        <dt>Wool</dt>
+        <dd>£{this.props.market.woolPrice} bags</dd>
+        <button onClick={() => this.sellItem("wool")}>Sell Wool</button>
+        <dt>Lamb</dt>
+        <dd>£{this.props.market.meatLambPrice} kilograms</dd>
+        <button onClick={() => this.sellItem("lamb")}>Sell Lamb</button>
+      </dl>
+    </div>
     )
   }
 }
